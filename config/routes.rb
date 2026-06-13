5869c08065a4d77c6800dcf42fd5e2e983e9f340
egifufox <acrobat.1205@gmail.com>
2026-06-14 02:03:28 +0900

    feature/#2 PR修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "static_pages#top"
+  root "user_sessions#new"
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
