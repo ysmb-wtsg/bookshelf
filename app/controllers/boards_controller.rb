1a6c632807aaf30bcba07abfe336a0070001727a
egifufox <acrobat.1205@gmail.com>
2026-07-12 13:28:42 +0900

    feature/#5 指摘に対する修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,7 @@ class BoardsController < ApplicationController
 
   def index
     @q = Board.ransack(params[:q])
-    @boards = @q.result(distinct: true).includes(:user, :reviews).page(params[:page]).per(PER_PAGE)
+    @boards = @q.result(distinct: true).includes(:user, :reviews, :tags).page(params[:page]).per(PER_PAGE)
   end
 
   def new
@@ -13,7 +13,6 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      save_tags(@board)
       redirect_to boards_path, success: '書籍を登録しました'
     else
       flash.now[:danger] = '書籍を登録できませんでした'
@@ -34,7 +33,6 @@ class BoardsController < ApplicationController
   def update
     @board = current_user.boards.find(params[:id])
     if @board.update(board_params)
-      save_tags(@board)
       redirect_to board_path(@board), success: '書籍を更新しました'
     else
       flash.now[:danger] = '書籍を更新できませんでした'
@@ -54,26 +52,6 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:title, :author, :body)
+    params.require(:board).permit(:title, :author, :body, tag_ids: [])
   end
-
-  #タグを保存するメソッド
-  def save_tags(board)
-
-    #送信されたデータが空であれば既存のタグを全て削除してreturnで終了
-    if params[:board][:tag_ids].blank?
-      board.board_tags.destroy_all
-      return
-    end
-
-    #既存のタグをクリア(updateアクション用で)
-    board.board_tags.destroy_all
-
-    #選択されたタグIDを1つずつ取り出して、書籍とタグを紐付けるレコードを作る
-    tag_ids = params[:board][:tag_ids].reject(&:blank?)
-    tag_ids.each do |tag_id|
-      board.board_tags.create(tag_id: tag_id)
-    end
-  end
-
 end
